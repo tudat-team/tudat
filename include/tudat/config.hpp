817e8075404ea35623f83a8576f3d29fb11aa3d3
Quinten E. van Woerkom <quintenvanwoerkom@gmail.com>
2023-04-10 23:25:17 +0200

    Manual implementation of "config.hpp"
    This should normally be done during Tudat build, but is not at this moment, for some reason.
    Manually inserting the correct values and copying it to the right location
    works too.

---
+++ include/tudat/config.hpp
@@ -0,0 +1,24 @@
+/*    Copyright (c) 2010-2018, Delft University of Technology
+ *    All rigths reserved
+ *
+ *    This file is part of the Tudat. Redistribution and use in source and
+ *    binary forms, with or without modification, are permitted exclusively
+ *    under the terms of the Modified BSD license. You should have received
+ *    a copy of the license with this file. If not, please or visit:
+ *    http://tudat.tudelft.nl/LICENSE.
+ */
+
+#ifndef TUDAT_CONFIG_HPP
+#define TUDAT_CONFIG_HPP
+
+#define TUDAT_VERSION "2.11.1"
+#define TUDAT_VERSION_MAJOR "2"
+#define TUDAT_VERSION_MINOR "11"
+#define TUDAT_VERSION_PATCH "1"
+
+// TODO: This need to be moved somewhere else eventually.
+#define TEST_DATA_FOLDER "home3/quinten/thesis/code/tudat/tests/data/"
+
+//@TUDAT_BUILD_TESTS@
+
+#endif
