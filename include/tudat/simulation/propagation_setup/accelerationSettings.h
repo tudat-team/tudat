622c201b90c0d872dc4b4049d62079bed588b4bc
Space Jonas <jonas.hener@web.de>
2025-07-21 10:23:41 +0200

    add factory function for rtg_acceleration type settings

--- include/tudat/simulation/propagation_setup/accelerationSettings.h
+++ include/tudat/simulation/propagation_setup/accelerationSettings.h
@@ -337,6 +337,7 @@ inline std::shared_ptr< AccelerationSettings > empiricalAcceleration(
     return std::make_shared< EmpiricalAccelerationSettings >( constantAcceleration, sineAcceleration, cosineAcceleration );
 }
 
+
 // Class to define settings for yarkovsky accelerations
 //! @get_docstring(YarkovskyAccelerationSettings.__docstring__)
 class YarkovskyAccelerationSettings : public AccelerationSettings
@@ -616,8 +617,8 @@ inline std::shared_ptr< AccelerationSettings > customAccelerationSettings(
 }
 
 
-//! @get_docstring(CustomAccelerationSettings.__docstring__)
-class   : public AccelerationSettings
+//! @get_docstring(RTGAccelerationSettings.__docstring__)
+class RTGAccelerationSettings : public AccelerationSettings
 {
 public:
     RTGAccelerationSettings( const Eigen::Vector3d& bodyFixedForceVectorAtReferenceEpoch,
@@ -631,6 +632,18 @@ public:
     const double decayScaleFactor_;
     const double referenceEpoch_;
 };
+
+
+//! @get_docstring(rtgAcceleration)
+inline std::shared_ptr< AccelerationSettings > rtgAcceleration(
+        const Eigen::Vector3d& bodyFixedForceVectorAtReferenceEpoch,
+        const double decayScaleFactor,
+        const double referenceEpoch )
+{
+    return std::make_shared< RTGAccelerationSettings >( bodyFixedForceVectorAtReferenceEpoch, decayScaleFactor, referenceEpoch );
+}
+
+
 
 // Class for providing settings for a direct tidal acceleration model, with approach of Lainey et al. (2007, 2009, ..)
 /*
